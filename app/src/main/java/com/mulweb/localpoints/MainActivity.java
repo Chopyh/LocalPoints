ab5a614140c04b350c04ce1561c5d4632901c59f
Chopyh <javierentrena2403@outlook.com>
2024-11-13 22:36:32 +0100

    Change project

--- app/src/main/java/com/mulweb/localpoints/MainActivity.java
+++ app/src/main/java/com/mulweb/localpoints/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView nav = findViewById(R.id.bottomNavigationView);
+        loadFragment(new MapFragment());
+        nav.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.nav_item_map) {
+                fragment = new MapFragment();
+            } else if (item.getItemId() == R.id.nav_item_list) {
+                fragment = new ListFragment();
+            } else if (item.getItemId() == R.id.nav_item_forum) {
+                fragment = new ForumFragment();
+            } else if (item.getItemId() == R.id.nav_item_settings) {
+                fragment = new SettingsFragment();
+            }
+
+            return loadFragment(fragment);
+        });
     }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
 }
